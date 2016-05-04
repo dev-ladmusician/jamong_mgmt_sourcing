07826c719842bda856500e4a973ce18448baf179
dev-ladmusician <dev-ladmusician@gmail.com>
2016-05-04 16:51:28 +0900

    commit for pull

--- application/controllers/content.php
+++ application/controllers/content.php
@@ -7,7 +7,7 @@ class Content extends CORE_Controller
         parent::__construct();
         //$this->__require_admin_login();
         $this->load->model('content_model');
-        $this->load->model('content_model');
+        $this->load->model('category_model');
         $this->load->model('content_model');
     }
 
